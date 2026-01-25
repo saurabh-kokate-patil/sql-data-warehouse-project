3a8f1c9bd371e9827223043f8e54cadce6824a47
saurabh-kokate-patil <saurabh.kokate.ai@gmail.com>
2026-01-25 18:46:48 +0530

    Add files via upload

---
+++ scripts/init_database.sql
@@ -0,0 +1,14 @@
+--Create Database 'DataWarehouse'
+
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+Go
+CREATE SCHEMA silver;
+Go
+CREATE SCHEMA gold;
+Go
